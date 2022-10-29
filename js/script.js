608eb156c83390fed21336a924dbc80e7918a54a
Elin <contact@elin.codes>
2022-10-29 03:48:47 -0400

    Added hamburger script files to both main and email pages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const hamburger = document.getElementById('hamburger');
 const navigation = document.getElementById('navigation');
+const menuLink = document.getElementById('menu-link');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navigation.classList.toggle('active');
-})
+});
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navigation.classList.remove('active');
+}));
